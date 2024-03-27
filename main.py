fd9fa41272f3323a0cadc52e8a9b8132ff398f7e
uzengo <21187455+uzengo@users.noreply.github.com>
2024-03-28 02:01:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -143,4 +143,4 @@ async def translate_request(chat_request: ChatRequest):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=80)
